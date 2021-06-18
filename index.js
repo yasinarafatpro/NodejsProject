d9e70e1edb93affb6a15bcfa0901a65e32c6610a
Yasin Arafat <yasinarafat1194@gmail.com>
2021-06-18 20:20:36 +0600

    validation and createuser

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const server=http.createServer(app);
 
 server.listen(3000,()=>{
     console.log('server is listening at port 3000');
-})
+});
--- src/app.js
+++ src/app.js
@@ -5,18 +5,12 @@ app.get('/hello_world', (req, res) => {
   console.log('requesr accepted');
   res.send({
     data: {
-      name: 'first_nodejs'
+      name: 'first_nodejs',
     },
   });
-};
-);
-app.post('/hello_post', (req, res) => {
-  console.log('requesr accepted',req.body);
-  res.send({
-    data: {
-      name: 'first_nodejs-post'
-    },
-  });
-};
-);
+});
+
+app.post('/post-request', (req, res) => {
+  console.log('post accepted',req.body);
+});
 module.exports = app;
